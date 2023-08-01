a2f020017d356c7700949346ef6123fa013afec2
Brodi LeBlanc <brodi.leblanc@gmail.com>
2023-07-31 21:56:06 -0700

    created about me page

---
+++ src/components/AboutMe.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to My About Me Page</h1>
+      </header>
+      <main>
+        <section>
+          <h2>About Me</h2>
+          <p>
+            {/* Add my personal information and introduction here */}
+          </p>
+        </section>
+      </main>
+      <footer>
+        <p>Contact me at: brodi.leblanc@gmail.com</p>
+      </footer>
+    </div>
+  );
+};
+
+export default AboutMe;
