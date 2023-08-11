3e7e04a24d661c231eb305f77125e130d3773579
Brodi LeBlanc <brodi.leblanc@gmail.com>
2023-08-11 13:08:39 -0700

    resume data

---
+++ src/components/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://",
+    "name": "Brodi LeBlanc",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like designing various parts of frontend development and like to learn about new technologies, shooting photos, learning spanish, or trying a new agave spirit in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/brodileblanc/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/brodi-xx",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/brodi__xx",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am recent Full-Stack Developer grad from UT's Coding Bootcamp. My current learning interest is AI technology and advancement. I have a firm belief in leaving things better than you found them.",
+    "address":"United States",
+    "website":"https://www.brodileblanc.com",
+    "education":[
+      {
+        "UniversityName":"The University of Texas",
+        "specialization":"Full-Stack Engineer",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2023",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"St. Edward's University",
+        "specialization":"Masters of Business Administration",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2015",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"GoDaddy",
+        "specialization":"Enterprise CSM",
+        "MonthOfLeaving":"Current",
+        "YearOfLeaving":"Current",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"GoDaddy",
+        "specialization":"Social Media Consultant",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Dev skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/placeholder.png"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/placeholder.png"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/placeholder.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/placeholder.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some tech guru"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some tech guru"
+      }
+    ]
+  }
+  
+  export default resumeData
